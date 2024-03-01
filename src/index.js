a9724bdfd710400c78e8f5a99dca45ee2e4ff0d4
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-01 09:08:02 +0100

    Create first component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn reasct js by building Taskopedia</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to lidl</li>
+      </ul>
+    </div>
+  );
+}
+
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
-);
-
+<div>
+  <MainBody/>
+</div>);
