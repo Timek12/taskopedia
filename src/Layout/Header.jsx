3b45542aebf9e9eae278faa835cf30a432146c6b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-02 21:51:39 +0100

    Refactor mainbody component, move components to layout folder

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,8 +1,8 @@
-import logo from "./images/react.png"
+import logo from "../images/react.png"
 function MainHeader() {
   return(
     <div className="pt-3 py-1 pl-2" style={{backgroundColor: "black"}}>
-      <img src={logo} style = {{ height: "35px", verticalAlign: "top"}}>
+      <img alt= "" src={logo} style = {{ height: "35px", verticalAlign: "top"}}>
       </img>
       <span className="h2 pt-4 text-white-50">React Course - Taskopedia</span>
     </div>
@@ -12,7 +12,7 @@ function MainHeader() {
 const subHeaderStyle = {
   color: "blueviolet",
   backgroundColor: "lightgrey",
-};
+}
 
 function SubHeader() {
   return <p style={subHeaderStyle}>This is an exciting course!</p>;
---
+++ src/MainBody.jsx
@@ -0,0 +1,40 @@
+import Student from "./Student";
+
+export default function MainBody() {
+  const whatWeWillLearn = "React JS";
+  const totalLectures = 3;
+  return (
+    <div style={{ minHeight: "70vh" }}>
+      <p className="text-primary">
+        In this course, we will learn {whatWeWillLearn} by building Taskopedia
+      </p>
+      <p>Total Lecture - {totalLectures}</p>
+      <ul>
+        <li style={{ color: "green" }}>Basic Foundation</li>
+        <li>Functional and class components</li>
+      </ul>
+
+      <div className="container row">
+        <Student
+          className="Agata Kowalska"
+          codingExperience={4}
+          headshot="https://api.lorem.space/image/face?w=150&h=150"
+        />
+        <Student
+          className="Agnieszka Wlodarczyk"
+          codingExperience={5}
+          headshot="https://api.lorem.space/image/face?w=150&h=151"
+        />
+        <Student
+          className="Kamila Bakala"
+          codingExperience={2}
+          headshot="https://api.lorem.space/image/face?w=150&h=153"
+        />
+      </div>
+
+      {/* <div>
+          Enter Task: <input maxLength={5} readOnly={false} placeholder="Timothy"></input>
+        </div> */}
+    </div>
+  );
+}
