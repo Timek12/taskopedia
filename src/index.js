a906c9642f94daf4c5d77e7b624036efc252abe2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-02 16:05:45 +0100

    Add student component with custom avatar image

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import "./CSS/style.css"
+import "./CSS/style.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function MainHeader() {
@@ -8,8 +8,8 @@ function MainHeader() {
 
 const subHeaderStyle = {
   color: "blueviolet",
-  backgroundColor: "lightgrey"
-}
+  backgroundColor: "lightgrey",
+};
 
 function SubHeader() {
   return <p style={subHeaderStyle}>This is an exciting course!</p>;
@@ -25,29 +25,58 @@ function Header() {
 }
 
 function MainBody() {
-
   const whatWeWillLearn = "React JS";
   const totalLectures = 3;
   return (
     <div>
-      <p className="text-primary">In this course, we will learn {whatWeWillLearn} by building Taskopedia</p>
+      <p className="text-primary">
+        In this course, we will learn {whatWeWillLearn} by building Taskopedia
+      </p>
       <p>Total Lecture - {totalLectures}</p>
       <ul>
-        <li style={{color: "green"}}>Basic Foundation</li>
+        <li style={{ color: "green" }}>Basic Foundation</li>
         <li>Functional and class components</li>
       </ul>
+      {/* <div>
+        Enter Task: <input maxLength={5} readOnly={false} placeholder="Timothy"></input>
+      </div> */}
+    </div>
+  );
+}
+
+function Students() {
+  const fullName = "Kris Walley";
+  const programmingExperience = 2;
+  return (
+    <div className="container p-4">
+      <div className="row">Students Enrolled</div>
+      <div className="row border">
+        <div className="col-2">
+          <img
+            src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className="w-100"
+          ></img>
+        </div>
+        <div className="col-10">
+          {fullName} <br />
+          Coding Experience {programmingExperience} years
+        </div>
+      </div>
     </div>
   );
 }
 
 function Footer() {
-  return <p style={{color:"grey", backgroundColor:"black"}} >Happy Coding!</p>;
+  return (
+    <p style={{ color: "grey", backgroundColor: "black" }}>Happy Coding!</p>
+  );
 }
 
 root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
