bd62ac945f62e34e0fd78398b6bad8f8892d3ae3
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-02 22:11:14 +0100

    Add cursor pointer to student rating icons

--- src/StudentReview.jsx
+++ src/StudentReview.jsx
@@ -1,8 +1,8 @@
 export default function StudentReview() {
     return( 
         <div className="p-2">
-        <i class="bi bi-hand-thumbs-up-fill text-success"></i> &nsbp;
-        <i class="bi bi-hand-thumbs-down-fill text-danger"></i>
+        <i class="bi bi-hand-thumbs-up-fill text-success p-1" style={{cursor: "pointer"}}></i> 
+        <i class="bi bi-hand-thumbs-down-fill text-danger p-1" style={{cursor: "pointer"}}></i>
         </div>
     );
 }
